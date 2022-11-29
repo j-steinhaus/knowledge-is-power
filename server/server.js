2566f288d476a2ccf7455e418004c77fd1df8056
j-steinhaus <jessicamichele11@gmail.com>
2022-11-28 19:06:52 -0700

    TA help - organizational

--- server/server.js
+++ server/server.js
@@ -1,47 +1,45 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const session = require("express-session")
-const path = require ("path");
-const routes = require();
-const passport = require("passport");
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+// const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-const PORT = process.env.port || 3001;
-
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
-
-// defining api routes and where request head to
-if (process.env.NODE_ENV === "production") {
-    app.all('*', function(req, res) {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
-  } else {
-    app.all('*', function(req, res) {
-      res.sendFile(path.join(__dirname, "./client/public/index.html"));
-    });
-  }
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// middleware 
-app.use(
-  session({ secret: "pineapples", resave: true, saveUninitialized: true })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-routes(app);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-//   connection 
-  app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
-  });
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+ };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
