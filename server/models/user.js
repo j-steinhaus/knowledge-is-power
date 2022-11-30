7dbb9ff5e5b4f059b9c0677e9fc81c16ddaeb6d1
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-11-29 20:09:33 -0600

    added index.js file in model folders and started on the user.js page in models

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,11 @@
 // info needing to save 
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema ({
+    name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    },
+})
