32bc3e2f0adeb802649c29bbd988c7265713d012
j-steinhaus <jessicamichele11@gmail.com>
2022-12-01 21:44:34 -0700

    who knows...

--- client/src/sections/Home.js
+++ client/src/sections/Home.js
@@ -1,7 +1,31 @@
+import Nav from "./Nav"
+// import "./login.css"
+
 const Home = () => {
     return (
         <>
-            <h1>Hello</h1>
+            <Nav/>
+            <form>
+      <div class="form-field" id="form-field-user">
+        <input type="text" id="" placeholder="Username" required />
+      </div>
+
+      <div class="form-field" id="form-field-pwd">
+        <input type="password" placeholder="Password" required />
+      </div>
+
+      <div class="form-field" id="form-field-btn">
+        <button
+          onclick="location.href='./signup.html'"
+          class="btn2"
+          id="createAccount"
+          type="submit"
+        >
+          Signup
+        </button>
+        <button class="btn" id="submit" type="submit">Login</button>
+      </div>
+    </form>
         </>
     )
 };
